8394fee0a33f18232359e023cbd85df68ad1579a
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-17 14:35:15 +0530

    path correction

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
-const p1 = filePaths[0]
 const filePaths = process.argv.slice(2)
+const p1 = filePaths[0]
 const p2 = filePaths[1]
 
 const joinPaths = (p1, p2) => path.join(p1, p2)
